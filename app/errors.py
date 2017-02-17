fb079b43de31b88ab9a9aed092b110a21438586c
kmbn <kmbn@sotomail.net>
2017-02-17 13:38:18 +0100

    Add new error templates

--- app/errors.py
+++ app/errors.py
@@ -2,6 +2,11 @@ from flask import render_template
 from . import app
 
 
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
