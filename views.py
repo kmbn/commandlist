e5033c921c59fdee1aa0d32e7a248ae0ca8ab53d
kmbn <kmbn@sotomail.net>
2017-02-17 12:03:07 +0100

    Add WTForms and inline form validation
    Streamline the way tasks are processed as well.

--- views.py
+++ views.py
@@ -5,12 +5,14 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from datetime import datetime
 from db import get_db, close_db
 from config import app
+from forms import NextActionForm
 
 
 @app.route('/')
 def main_view():
     '''Fetch the current user's tasks and calculate the size of the bucket.'''
     current_user = 1
+    form = NextActionForm()
     db = get_db()
     cur = db.execute('select id, description from tasks where creator_id = ? \
                      order by id asc limit 5', (current_user,))
@@ -26,7 +28,7 @@ def main_view():
         missing_rows.append('item')
     limit = len(tasks) # Necessary for parsing input in get_next_action
     session['limit'] = limit
-    session['tasks'] = task_ids
+    session['task_ids'] = task_ids
     db = get_db()
     cur = db.execute('select count(id) from tasks where creator_id = ?', \
                      (current_user,))
@@ -35,7 +37,7 @@ def main_view():
         bucket = bucket[0]
         bucket -= 5
     return render_template('main.html', tasks=tasks, bucket=bucket, \
-                           missing_rows=missing_rows)
+                           missing_rows=missing_rows, form=form)
 
 
 @app.route('/get_next_action', methods=['GET', 'POST'])
@@ -48,23 +50,40 @@ def get_next_action():
     4. Wipe the list and start over ('reset list')
     5. Redirect the user to a different page (e.g., 'help' will redirect
     to the how-to page; 'back' will redirect to the homepage)
+    '''
+    form = NextActionForm()
+    if form.validate_on_submit():
+        next_action = form.next_action.data[0].upper() + form.next_action.data[1:]
+        print(next_action)
+        if next_action[0] == 'C':
+            try:
+                int(next_action[1]) == int
+            except ValueError:
+                print('huh')
+                return add_task(next_action)
+            return clear_task(next_action[:2])
+        elif next_action[:3] == 'Rev':
+            try:
+                int(next_action[3]) == int
+            except ValueError:
+                return add_task(next_action)
+            return revise(next_action)
+        elif next_action == 'Reset list':
+            return restart()
+        elif next_action == 'Help':
+            return redirect(url_for('how_to'))
+        elif next_action == 'Back':
+            return redirect(url_for('main_view'))
+        else:
+            return add_task(next_action)
+    return redirect(url_for('main_view'))
 
-    The limit is used ensure that a user is referring to an actual position
-    on the task list.'''
-    if request.method != 'POST':
-        return redirect(url_for('main_view'))
-    limit = session.get('limit')
-    next_action = request.form['next_action']
-    if len(next_action) == 0:
-        flash('You have to type something in the box ;\).')
-        return redirect(url_for('main_view'))
-    next_action = next_action[0].upper() + next_action[1:]
-    session['next_action'] = next_action
+'''
     if next_action[0] == 'C':
         try:
             int(next_action[1]) == int
         except ValueError:
-            return check_task()
+            return check_task() # Check task is now part of the validation
         if 0 < int(next_action[1]) <= limit:
             return clear_task()
         elif int(next_action[1]) > limit:
@@ -76,7 +95,7 @@ def get_next_action():
         try:
             int(next_action[3]) == int
         except ValueError:
-            return check_task()
+            return check_task()  # Check task is now part of the validation
         if 0 < int(next_action[3]) <= limit:
             return revise()
         elif int(next_action[3]) > limit:
@@ -87,26 +106,11 @@ def get_next_action():
     elif next_action == 'Back' or next_action == "'back'":
         return redirect(url_for('main_view'))
     else:
-        return check_task()
+        return check_task()  # Check task is now part of the validation
+'''
 
-
-def check_task():
-    '''Verify that the task is not a duplicate.'''
-    next_action = session.get('next_action')
-    db = get_db()
-    cur = db.execute('select description from tasks where description = ?', \
-                     (next_action,))
-    check = cur.fetchone()
-    if check is None:
-        return add_task()
-    else:
-        flash('That task is already on your list.')
-        return redirect(url_for('main_view'))
-
-
-def add_task():
+def add_task(next_action):
     '''Add the task to the task table.'''
-    next_action = session.get('next_action')
     current_user = 1
     current_time = datetime.utcnow()
     db = get_db()
@@ -116,12 +120,11 @@ def add_task():
     return redirect(url_for('main_view'))
 
 
-def clear_task():
+def clear_task(next_action):
     '''Delete the task from the task table.'''
-    next_action = session.get('next_action')
-    tasks = session.get('tasks')
+    task_ids = session.get('task_ids')
     task_number = int(next_action[1])
-    task_id = tasks[task_number-1]
+    task_id = task_ids[task_number-1]
     db = get_db()
     db.execute('delete from tasks where id = ?', (task_id,))
     db.commit()
@@ -138,26 +141,22 @@ def restart():
     return redirect(url_for('main_view'))
 
 
-def revise():
+def revise(next_action):
     '''Revise the given task (indicated by index[3] of the input).
     All text from index[5] onward is considered to be the new content
     of the task.'''
-    next_action = session.get('next_action')
-    tasks = session.get('tasks')
-    if len(next_action) > 5:
-        task_number = int(next_action[3])
-        task_id = tasks[task_number-1]
-        new_task = next_action[4:]
-        db = get_db()
-        db.execute('update tasks set description = ? where id = ?', \
-                   (new_task, task_id))
-        db.commit()
-        return redirect(url_for('main_view'))
-    else:
-        flash('Enter the revised task on the same line.')
-        return redirect(url_for('main_view'))
+    task_ids = session.get('task_ids')
+    task_number = int(next_action[3])
+    task_id = task_ids[task_number-1]
+    new_task = next_action[4:]
+    db = get_db()
+    db.execute('update tasks set description = ? where id = ?', \
+               (new_task, task_id))
+    db.commit()
+    return redirect(url_for('main_view'))
 
 
 @app.route('/how_to')
 def how_to():
-    return render_template('how_to.html')
+    form = NextActionForm()
+    return render_template('how_to.html', form=form)
